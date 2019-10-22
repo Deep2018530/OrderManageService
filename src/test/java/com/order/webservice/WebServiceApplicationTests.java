47a4d2519cb1d84662908976a380d04476a184bc
zhangdingping <zhangdingping@oceanum.cn>
2019-10-22 15:39:47 +0800

    项目搭建

---
+++ src/test/java/com/order/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
