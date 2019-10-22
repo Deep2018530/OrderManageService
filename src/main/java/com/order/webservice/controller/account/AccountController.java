0e691319cc5fb477ed3d7524d50a57572f4241ee
zhangdingping <zhangdingping@oceanum.cn>
2019-10-22 18:23:20 +0800

    控制器

---
+++ src/main/java/com/order/webservice/controller/account/AccountController.java
@@ -0,0 +1,13 @@
+package com.order.webservice.controller.account;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/account")
+@Api(value = "账户API接口")
+public class AccountController {
+
+}
---
+++ src/main/java/com/order/webservice/controller/user/UserController.java
@@ -0,0 +1,12 @@
+package com.order.webservice.controller.user;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+@Api(value = "用户API接口")
+public class UserController {
+}
