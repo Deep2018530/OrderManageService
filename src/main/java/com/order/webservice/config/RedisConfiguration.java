a94c5c1e4a2e1d5502ede2cbdb025f104500da52
Deep2018530 <1184858443@qq.com>
2019-10-27 11:05:11 +0800

    redis“

--- src/main/java/com/order/webservice/config/RedisConfiguration.java
+++ src/main/java/com/order/webservice/config/RedisConfiguration.java
@@ -3,7 +3,10 @@ package com.order.webservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * created by zhangdingping on 2019/10/27
@@ -11,13 +14,5 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 @Configuration
 public class RedisConfiguration {
 
-    private RedisConnectionFactory connectionFactory = null;
 
-    @Bean(name = "RedisConnectionFactory")
-    public RedisConnectionFactory initRedisConnectionFactory() {
-        if (this.connectionFactory != null) {
-            return this.connectionFactory;
-        }
-
-    }
 }
