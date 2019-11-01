9acc6a6632c817b19dd0bc0e5a03a3b6d18af7f0
hehaotian <hht1026@foxmail.com>
2019-11-01 16:17:19 +0800

    订单

--- src/main/java/com/order/webservice/controller/order/OrderController.java
+++ src/main/java/com/order/webservice/controller/order/OrderController.java
@@ -1,6 +1,5 @@
 package com.order.webservice.controller.order;
 
-
 import com.order.webservice.common.HttpResult;
 import com.order.webservice.domain.dto.order.OrderDto;
 import com.order.webservice.domain.vo.PageResponseVo;
@@ -12,7 +11,6 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping(value = "/order")
 @Api(value = "订单查询API接口")
@@ -22,15 +20,6 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping("/order/{page}/{size}")
-    @ApiOperation(value = "管理员订单展示")
-    public HttpResult<PageResponseVo<OrderVo>> getOrderPage(@ApiParam(value = "第几页（第一页是1)")
-                                                            @PathVariable(value = "page", required = false) Integer page,
-                                                            @ApiParam(value = "每页条数")
-                                                            @PathVariable(value = "size", required = false) Integer size) {
-        return HttpResult.success(orderService.getOrderPage(page, size));
-    }
-
-    @GetMapping("/order/query/{page}/{size}")
     @ApiOperation(value = "管理员订单查询")
     public HttpResult<PageResponseVo<OrderVo>> query(@ApiParam(value = "第几页（第一页是1)")
                                                      @PathVariable(value = "page", required = false) Integer page,
