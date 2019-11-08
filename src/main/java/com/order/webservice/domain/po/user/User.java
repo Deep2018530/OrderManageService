16423c772e0697c46cb56269cf94e1e73de34673
zhangdingping <zhangdingping@oceanum.cn>
2019-11-08 17:17:17 +0800

    。。“

--- src/main/java/com/order/webservice/domain/po/user/User.java
+++ src/main/java/com/order/webservice/domain/po/user/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = -6125297654796395674L;
 
-    @TableId(value = "id", type = IdType.INPUT)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     private String nickName;
